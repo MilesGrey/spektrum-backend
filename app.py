b5fb33fecfaa2ba35878ec178a4cb9b8f48d81a7
Benjamin Jin <benjaminmjin@gmail.com>
2021-12-06 12:41:47 +0100

    Dockerize server

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ app.register_blueprint(RESULT_API, url_prefix='/result')
 app.register_blueprint(SPEAKER_API, url_prefix='/speaker')
 app.register_blueprint(VIEW_API, url_prefix='/view')
 
-cred = credentials.Certificate(join(dirname(dirname(__file__)), 'serviceAccountKey.json'))
+cred = credentials.Certificate(join(dirname(dirname(__file__)), '/app/serviceAccountKey.json'))
 firebase_admin.initialize_app(cred)
 
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=1234, debug=True)
+    serve(app, host='0.0.0.0', port=80)
